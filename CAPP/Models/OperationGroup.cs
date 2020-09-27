8cf4004e8bc57ab221ecea6f0a9473844098902b
Slava <slava4973@mail.ru>
2020-09-27 23:09:30 +0300

    init

---
+++ CAPP/Models/OperationGroup.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CAPP.Models
+{
+    [Table("OperationGroups")]
+    public class OperationGroup
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Key]
+        [Column(TypeName = "NVARCHAR(255)")]
+        public string Code { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(255)")]
+        public string Name { get; set; }
+    }
+}
---
+++ CAPP/Models/OperationObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CAPP.Models
+{
+    [Table("OperationObjects")]
+    public class OperationObject
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Key]
+        [Column(TypeName = "NVARCHAR(255)")]
+        public string Code { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(255)")]
+        public string Name { get; set; }
+
+        public virtual ICollection<OperationOperationObject> OperationOperationObjects { get; set; }
+    }
+}
